f3413a75446df62c63c10d50c8dc6b3b5f83c37c
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-04-28 04:28:26 +0000

    index.html

--- python.py
+++ python.py
@@ -1,14 +1,14 @@
 print("Welcome!")
 def findpi():
   try:
-    inputnumber=int(input("Please enter a number to estimate the digits of PI:"))
+    inputnumber=int(input("Please enter a number to estimate the digits of pi:"))
     pi = 0
     for n in range (inputnumber):
       value = ((-1)**n)/(2*n+1)
       pi = pi + value
       n = n+1
     pi = pi*4
-    return ("Your estimated value of PI is: \n" + str(pi))
+    return ("Your estimated value of pi is: \n" + str(pi))
   except:
     return("Error! \nPlease enter a positive whole number. \nLetters or words, negative numbers and decimals are not allowed.")
 result=findpi()
